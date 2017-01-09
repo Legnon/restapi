2c1e9711b1681f72b19a107d6574ef4b6357d776
KongHyeontaek <rotanev7@gmail.com>
2017-01-09 17:17:06 +0900

    add foreign key(user) on snippets
    models = foreignkey
    serializers = UserSerializer
    urls = users
    views = userList, userDetail (for read-only)

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,6 +1,24 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 # from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 from .models import Snippet
+
+
+class SnippetSerializer(serializers.ModelSerializer):
+	owner = serializers.ReadOnlyField(source='owner.username')
+
+	class Meta:
+		model = Snippet
+		fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+
+
+class UserSerializer(serializers.ModelSerializer):
+	snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+	class Meta:
+		model = User
+		fields = ('id', 'username', 'snippets')
+
 
 # class SnippetSerializer(serializers.Serializer):
 # 	id = serializers.IntegerField(read_only=True)
@@ -29,8 +47,3 @@ from .models import Snippet
 # 		instance.save()
 # 		return instance
 
-
-class SnippetSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Snippet
-		fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
