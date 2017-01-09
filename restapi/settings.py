7b041609649069475ceb31eb66837148c57e3732
KongHyeontaek <rotanev7@gmail.com>
2017-01-09 15:49:58 +0900

    function based view with rest framework

--- restapi/settings.py
+++ restapi/settings.py
@@ -127,9 +127,9 @@ STATIC_URL = '/static/'
 
 # rest_framework
 
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAdminUser',
-    ],
-    'PAGE_SIZE': 10
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_PERMISSION_CLASSES': [
+#         'rest_framework.permissions.IsAdminUser',
+#     ],
+#     'PAGE_SIZE': 10
+# }
